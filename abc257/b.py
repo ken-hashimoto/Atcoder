57713f5769dd136b3e3592b889aa663e52d3bc95
hashimoto-k <hashimoto.kenji.1102@gmail.com>
2022-06-28 11:33:28 +0900

    簡潔に書き直した

--- abc257/b.py
+++ abc257/b.py
@@ -2,13 +2,10 @@ N,K,Q = map(int,input().split())
 A = list(map(int,input().split()))
 L = list(map(int,input().split()))
 
-now = [False]*(N+1)
-for i in range(K):
-  now[A[i]] = True
+A.append(N+1)
 for i in range(Q):
-  koma = A[L[i]-1]
-  if koma < N and now[koma+1] == False:
-    now[koma] = False
-    A[L[i]-1] += 1
-    now[koma+1] = True
-print(*A)
+  l = L[i]
+  l -= 1
+  if A[l+1] != A[l] + 1:
+    A[l] = A[l] + 1
+print(*A[:-1])
