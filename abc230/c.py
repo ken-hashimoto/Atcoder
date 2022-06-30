6956973b7329dbd94c969281a78a8b7bd1354f14
hashimoto-k <hashimoto.kenji.1102@gmail.com>
2022-06-30 11:28:54 +0900

    文字列の+=は遅いので修正

--- abc230/c.py
+++ abc230/c.py
@@ -1,16 +1,10 @@
 N,A,B = map(int,input().split())
-
-def is_black(i,j):
-  if i-j == A-B or i+j == A+B:
-    return True
-  return False
-
 P,Q,R,S = map(int,input().split())
 for i in range(P,Q+1):
-  ans = ""
+  ans = []
   for j in range(R,S+1):
     if i-j == A-B or i+j == A+B:
-      ans += "#"
+      ans.append("#")
     else:
-      ans += "."
-  print(ans)
+      ans.append(".")
+  print(''.join(ans))
