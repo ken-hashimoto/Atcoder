74999bea7c832a38c88c12d804b18b3477cbdc7f
hashimoto-k <hashimoto.kenji.1102@gmail.com>
2022-07-02 18:45:30 +0900

    BFS

---
+++ abc232/d.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+H,W = map(int,input().split())
+G = [list(input()) for _ in range(H)]
+dist = [[-1]*W for _ in range(H)]
+d = deque()
+d.append((0,0))
+dist[0][0] = 1
+ans = 1
+while d:
+  x,y = d.popleft()
+  if x+1 <W:
+    if G[x+1][y] != "#" and dist[x+1][y] == -1:
+      dist[x+1][y] = dist[x][y]+1
+      ans = max(dist[x+1][y],ans)
+      d.append((x+1,y))
+  if y+1 <H:
+    if G[x][y+1] != "#" and dist[x][y+1] == -1:
+      dist[x][y+1] = dist[x][y]+1
+      ans = max(dist[x][y+1],ans)
+      d.append((x,y+1))
+print(ans)
